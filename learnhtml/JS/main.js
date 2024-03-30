015ad4a4871fe9ed0c2d523076480ef1821ec590
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2024-03-30 06:31:48 +0530

    Added new function (#5)
    * First Commit
    
    * First Html page
    
    * Addded Paragraph
    
    * Added Second Paragraph
    
    * Added Java Function
    
    * added new function
    
    ---------
    
    Co-authored-by: bhatinti <163954341+bhatinti@users.noreply.github.com>

--- learnhtml/JS/main.js
+++ learnhtml/JS/main.js
@@ -1,3 +1,10 @@
 function myFunction(){
     document.getElementById("para1").innerHTML="Hello Javascript"
 }
+function addNumbers(){
+    let x=5;
+    let y=10;
+    let z= x+y;
+    document.getElementById("addnum").innerHTML= z
+
+}
