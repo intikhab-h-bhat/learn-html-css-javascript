1f0e32dcc67f2c2be3c2597939dc6a97766b0bd7
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2024-04-06 06:54:24 +0530

    added a class in js file

--- learnhtml/JS/main.js
+++ learnhtml/JS/main.js
@@ -1,10 +1,22 @@
-function myFunction(){
-    document.getElementById("para1").innerHTML="Hello Javascript"
+function myFunction() {
+  document.getElementById("para1").innerHTML = "Hello Javascript";
 }
-function addNumbers(){
-    let x=5;
-    let y=10;
-    let z= x+y;
-    document.getElementById("addnum").innerHTML= z
+function addNumbers() {
+  let x = 5;
+  let y = 10;
+  let z = x + y;
+  document.getElementById("addnum").innerHTML = z;
+}
 
-}
+class Car {
+  constructor(name,year) {
+    this.name=name;
+    this.year=year;
+  }
+  age() {
+    const date=new Date();
+    return date.getFullYear() - this.year;
+  }
+  }
+  const car1 = new Car("Ford",2014);
+  document.getElementById("para3").innerHTML = car1.age();
