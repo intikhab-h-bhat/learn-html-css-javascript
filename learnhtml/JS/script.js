ae75852ea005e9ca9a6812ebf8bae75ae8921166
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2024-04-14 16:05:04 +0530

    condition in js

--- learnhtml/JS/script.js
+++ learnhtml/JS/script.js
@@ -4,4 +4,17 @@ for(i in car) {
 console.log(car[i])
 }
 
+// javascript template string
+let x = 'Intikhab'
+
+console.log(`hello ${x}`)
+
+//javascript if else condition 
+
+let age = 14;
+if(age >= 14)
+    console.log("you are eligible for voting");
+else
+    console.log("You are not eligible");
+
 
