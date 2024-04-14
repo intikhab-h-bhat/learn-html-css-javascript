0a18a2b86900ca5d40fe9ef84fec1f6fc03e3d29
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-04-14 17:24:49 +0530

    switch statement

--- learnhtml/JS/script.js
+++ learnhtml/JS/script.js
@@ -1,32 +1,16 @@
-//terinary operator
-let y = 0;
-let isactive = y == 0 ? "login" : "logout"
-document.write(`please  ${isactive}`)
-
-
-//arrays in js
-const car = ["Maruti", "Ford", "Espresso", "Ambasdor"];
-for (i in car) {
-  console.log(car[i]);
+let input;
+input = 'n';
+switch(input){
+    case 1:
+    case "y":
+    case "yes":
+        document.write("Continue");
+        break;
+    case 0:
+    case "n":
+    case "no":
+        document.write("end");
+        break;
+    default:
+        document.write("Enter the correct value")
 }
-
-// javascript template string
-let x = "Intikhab";
-
-console.log(`hello ${x}`);
-
-//javascript if else condition
-
-let age = 14;
-hasvotercard = false;
-if (age >= 14 && hasvotercard == true) {
-  console.log("you are eligible for voting");
-} else if (age >= 14 && hasvotercard == false) {
-  console.log("get voter card");
-} else {
-  console.log("You are not eligible");
-}
-
-
-
-
