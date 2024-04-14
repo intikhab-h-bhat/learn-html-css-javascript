45f5e9c20be225e212aa37efff15cd201c902ec8
intikhab-h-bhat <44907729+intikhab-h-bhat@users.noreply.github.com>
2024-04-14 16:07:11 +0530

    condition in js (#34)
    * fetch method
    
    * javascript variable
    
    * arrays in js
    
    * condition in js

--- learnhtml/JS/script.js
+++ learnhtml/JS/script.js
@@ -5,3 +5,18 @@ console.log(car[i])
 }
 
 
+// javascript template string
+let x = 'Intikhab'
+
+console.log(`hello ${x}`)
+
+//javascript if else condition 
+
+let age = 14;
+if(age >= 14)
+    console.log("you are eligible for voting");
+else
+    console.log("You are not eligible");
+
+
+
