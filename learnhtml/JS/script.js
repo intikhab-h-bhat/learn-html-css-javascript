ca99fff91fd51d1ee8b5381aa8ee2549615dec91
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-04-16 21:33:18 +0530

    while loop

--- learnhtml/JS/script.js
+++ learnhtml/JS/script.js
@@ -1,17 +1,25 @@
-
 let input;
-input = 'n';
-switch(input){
-    case 1:
-    case "y":
-    case "yes":
-        document.write("Continue");
-        break;
-    case 0:
-    case "n":
-    case "no":
-        document.write("end");
-        break;
-    default:
-        document.write("Enter the correct value")
+input = "n";
+switch (input) {
+  case 1:
+  case "y":
+  case "yes":
+    document.write("Continue");
+    break;
+  case 0:
+  case "n":
+  case "no":
+    document.write("end");
+    break;
+  default:
+    document.write("Enter the correct value");
+}
+
+// Loops concept
+
+let count = 0;
+while (count < 10) {
+  console.log("Hello");
+ 
+  count++;
 }
