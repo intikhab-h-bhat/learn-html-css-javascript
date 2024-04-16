78c00b146c0511a6325f39eb3b860e10c0118a1c
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-04-16 21:41:49 +0530

    even odd number

--- learnhtml/JS/script.js
+++ learnhtml/JS/script.js
@@ -17,9 +17,16 @@ switch (input) {
 
 // Loops concept
 
-let count = 0;
+let count = 1;
 while (count < 10) {
-  console.log("Hello");
+  if(count % 2 ==0){
+    console.log(`${count}` + "is even number")
+  }
+  else
+  {
+    console.log(`${count}` + "is odd number")
+  }
+
  
   count++;
 }
