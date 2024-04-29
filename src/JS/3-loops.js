f777b298a0b7bc03947fa1c6201b642299a3ac18
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-04-29 09:25:09 +0530

    loops

---
+++ src/JS/3-loops.js
@@ -0,0 +1,38 @@
+// program to find the sum of n natural numbers
+// For Loop
+    // let n = prompt("Enter the value of n");
+    // let sum = 0;
+    // n = Number.parseInt(n);
+    // for (let i = 0; i < n; i++) {
+    // sum += i + 1;
+    // console.log(sum);
+    // }
+    //alert(sum);
+
+// For IN Loop used to get the key of object
+let stuMarks={
+    "abdullah":75,
+    "Mohammad":80,
+    "Zaid":75
+}
+for(let stu in stuMarks){
+    console.log(`${stu} marks obtained:`,stuMarks[stu] )
+}
+
+// For of Loop used to get the value of an object and is used in arrays
+for( stu of "Ahmad"){
+    console.log(stu)
+}
+
+
+// While and do while loop
+
+    let n = prompt("Enter the value of n");
+    let sum = 0;
+    let i=0;
+    n = Number.parseInt(n);
+    while(i <n){
+        sum+=i;
+        i++;
+    }
+    console.log(sum)
