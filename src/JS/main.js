f716813f210a264b24343f886ba9fb8955dc10d1
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-04-29 07:54:39 +0530

    assignement,comparison, logical operators

--- src/JS/main.js
+++ src/JS/main.js
@@ -23,33 +23,53 @@ const person = {
   name: "Abdullah",
   age: 15,
   dob: "04/12/2009",
-}
-console.log(person)
+};
+console.log(person);
 // person = 45 this gives error because person is constant variable
-person.name="Ahmad"
-person["friend"] = "Tabeen"
-console.log(person)
+person.name = "Ahmad";
+person["friend"] = "Tabeen";
+console.log(person);
 
 //Expression and Operators
-let a=50;
-let b=30;
-console.log(`${a} + ${b} = `, a + b)//Addition
-console.log(`${a} - ${b} = `, a - b)//Subtraction
-console.log(`${a} / ${b} = `, a / b)//Division
-console.log(`${a} % ${b} = `, a % b)//Modulas
-console.log(`${a} * ${b} = `, a * b)//multplication
-console.log(`${a} ** ${b} = `, a ** b)//Exponential
+let a = 50;
+let b = 30;
+console.log(`${a} + ${b} = `, a + b); //Addition
+console.log(`${a} - ${b} = `, a - b); //Subtraction
+console.log(`${a} / ${b} = `, a / b); //Division
+console.log(`${a} % ${b} = `, a % b); //Modulas
+console.log(`${a} * ${b} = `, a * b); //multplication
+console.log(`${a} ** ${b} = `, a ** b); //Exponential
 
 // Increament and Decreament operator
-console.log(a++,"increamented by 1");
+console.log(a++, "increamented by 1");
 console.log(a);
 
-console.log(++a) 
+console.log(++a);
 
-console.log(a--,'Decreament by 1');
+console.log(a--, "Decreament by 1");
 console.log(a);
 
-console.log(--a) 
+console.log(--a);
 
+// Assignment opertors
+let c = 8;
+console.log(c);
+c += 5;
+console.log(c);
 
+c -= 2;
 
+// Comparison opertors
+let comp1=6
+let comp2="6"
+console.log(`${comp1} equals ${comp2}`,comp1 == comp2 )
+console.log(`${comp1} not equals ${comp2}`,comp1 != comp2 )
+console.log(`${comp1} === ${comp2}`,comp1 === comp2 )
+console.log(`${comp1} !== ${comp2}`,comp1 !== comp2 )
+
+// logical operators
+let num1=5
+let num2=6
+let num3=5
+console.log(num1==num2 && num1==num3)
+console.log(num1==num2 || num1==num3)
