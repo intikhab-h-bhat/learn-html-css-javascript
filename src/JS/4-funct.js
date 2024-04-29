06c0433168ab3709e96b0aea7848f14d718f8016
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-04-29 09:52:51 +0530

    functions

---
+++ src/JS/4-funct.js
@@ -0,0 +1,25 @@
+// Functions
+// lets add two numbers
+
+function addTwoNum(a, b) {
+  return a + b;
+}
+
+console.log(addTwoNum(7, 5));
+
+//Arrow function
+
+    // let hello = () => {
+    // return "Welecome to Arrow Function";
+    // };
+    // console.log(hello());
+
+//Add two numbers using arrow functon
+
+    const addNumbers=(num1,nmu2)=>{
+        return num1+nmu2;
+    };
+
+    let x = addNumbers(3,4)
+    console.log(x)
+    
