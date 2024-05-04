83b6d2f398b7ea855e339ad6c9fadc9f9263679e
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-05-04 07:35:27 +0530

    strings

--- src/JS/4-funct.js
+++ src/JS/4-funct.js
@@ -26,37 +26,48 @@ console.log(addTwoNum(7, 5));
 
     ///
 
-    function isPalindrome(inputString) {
-        // Convert the input string to lowercase
-        inputString = inputString.toLowerCase();
-        // Initialize two pointers
-        let left = 0;
-        let right = inputString.length - 1;
-        // Move the pointers towards the middle
-        while (left < right) {
-            // Skip non-alphanumeric characters from both ends
-            while (left < right && !isAlphaNumeric(inputString[left])) {
-                left++;
-            }
-            while (left < right && !isAlphaNumeric(inputString[right])) {
-                right--;
-            }
-            // Compare characters at the pointers
-            if (inputString[left] !== inputString[right]) {
-                return false; // Not a palindrome
-            }
-            // Move the pointers inward
-            left++;
-            right--;
-        }
-        return true; // Palindrome
-    }
-    function isAlphaNumeric(char) {
-        return /[a-z0-9]/.test(char);
-    }
-    // Test the function
-    let inputString = "A man, a plan, a canal, Panama!";
-    console.log(`Is "${inputString}" a palindrome? ${isPalindrome(inputString)}`);
-    inputString = "kaYak";
-    console.log(`Is "${inputString}" a palindrome? ${isPalindrome(inputString)}`);
+    // function isPalindrome(inputString) {
+    //     // Convert the input string to lowercase
+    //     inputString = inputString.toLowerCase();
+    //     // Initialize two pointers
+    //     let left = 0;
+    //     let right = inputString.length - 1;
+    //     // Move the pointers towards the middle
+    //     while (left < right) {
+    //         // Skip non-alphanumeric characters from both ends
+    //         while (left < right && !isAlphaNumeric(inputString[left])) {
+    //             left++;
+    //         }
+    //         while (left < right && !isAlphaNumeric(inputString[right])) {
+    //             right--;
+    //         }
+    //         // Compare characters at the pointers
+    //         if (inputString[left] !== inputString[right]) {
+    //             return false; // Not a palindrome
+    //         }
+    //         // Move the pointers inward
+    //         left++;
+    //         right--;
+    //     }
+    //     return true; // Palindrome
+    // }
+    // function isAlphaNumeric(char) {
+    //     return /[a-z0-9]/.test(char);
+    // }
+    // // Test the function
+    // let inputString = "A man, a plan, a canal, Panama!";
+    // console.log(`Is "${inputString}" a palindrome? ${isPalindrome(inputString)}`);
+    // inputString = "kaYak";
+    // console.log(`Is "${inputString}" a palindrome? ${isPalindrome(inputString)}`);
     
+
+
+    // Function to find mean of 5 numbers
+
+    const mean = (num1,num2,num3,num4,num5)=>{
+
+        return (num1+num2+num3+num4+num5)/5 ;
+
+    }
+
+    console.log("Mean : " + mean(2,3,4,5,6))
---
+++ src/JS/5-strings.js
@@ -0,0 +1,13 @@
+//string using ""
+let name="Ahmad";
+//string using ''
+let fname='ahmad';
+console.log(name,fname)
+//get the length of the string
+console.log(name.length)
+
+//Template string
+let  num1=2;
+let num2 =3;
+let num3 =num1+num2
+console.log(`${num1}  + ${num2} is ${num3}`)
