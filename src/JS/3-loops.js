84585e04fea6e8d80785db323103bff80c08d529
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-05-04 07:12:01 +0530

    guess number

--- src/JS/3-loops.js
+++ src/JS/3-loops.js
@@ -1,55 +1,72 @@
 // program to find the sum of n natural numbers
 // For Loop
-    // let n = prompt("Enter the value of n");
-    // let sum = 0;
-    // n = Number.parseInt(n);
-    // for (let i = 0; i < n; i++) {
-    // sum += i + 1;
-    // console.log(sum);
-    // }
-    //alert(sum);
+// let n = prompt("Enter the value of n");
+// let sum = 0;
+// n = Number.parseInt(n);
+// for (let i = 0; i < n; i++) {
+// sum += i + 1;
+// console.log(sum);
+// }
+//alert(sum);
 
 // For IN Loop used to get the key of object
-let stuMarks={
-    "abdullah":75,
-    "Mohammad":80,
-    "Zaid":75
-}
-for(let stu in stuMarks){
-    console.log(`${stu} marks obtained:`,stuMarks[stu] )
+let stuMarks = {
+  abdullah: 75,
+  Mohammad: 80,
+  Zaid: 75,
+};
+for (let stu in stuMarks) {
+  console.log(`${stu} marks obtained:`, stuMarks[stu]);
 }
 
 // For of Loop used to get the value of an object and is used in arrays
-for( stu of "Ahmad"){
-    console.log(stu)
+for (stu of "Ahmad") {
+  console.log(stu);
 }
-
 
 // While and do while loop
 
-    let n = prompt("Enter the value of n");
-    let sum = 0;
-    let i=0;
-    n = Number.parseInt(n);
-    while(i <n){
-        sum+=i;
-        i++;
-    }
-    console.log(sum)
+let n = prompt("Enter the value of n");
+let sum = 0;
+let i = 0;
+n = Number.parseInt(n);
+while (i < n) {
+  sum += i;
+  i++;
+}
+console.log(sum);
 
+//**********Practise examples
+//Example to grt the marks of students
 
-    //**********Practise examples
-    //Example to grt the marks of students
+const marks = {
+  Ahmad: 90,
+  Sareena: 95,
+  Azka: 65,
+};
 
-    const marks={
-        "Ahmad":90,
-        "Sareena":95,
-        "Azka":65
-    }
+// simple for loop
+for (let i = 0; i < Object.keys(marks).length; i++) {
+  console.log(
+    "Marks of " + Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]]
+  );
+}
 
-    for(let i=0;i<Object.keys(marks).length;i++){
-        console.log("Marks of "+ Object.keys(marks)[i] + " are " + marks[Object.keys(marks)[i]])
-    } 
-
-    const myHeading = document.querySelector("h1");
+const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+// For In LOOP
+for (let key in marks) {
+  console.log("Marks of " + key + " are " + marks[key]);
+}
+
+// print try again untill yhe user enters correct number
+
+let cn = 5;
+let num;
+
+while (num != cn) {
+  console.log("try again");
+  num = prompt("Enter the number");
+}
+console.log("You enetered correct number");
