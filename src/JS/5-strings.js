ea30ee08b2b6d19cde8941036d6b3175c8cb1c12
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-05-04 07:59:41 +0530

    string methods

--- src/JS/5-strings.js
+++ src/JS/5-strings.js
@@ -1,13 +1,24 @@
 //string using ""
-let name="Ahmad";
+let name = "Ahmad";
 //string using ''
-let fname='ahmad';
-console.log(name,fname)
-//get the length of the string
-console.log(name.length)
+let fname = "ahmad";
+console.log(name, fname);
 
 //Template string
-let  num1=2;
-let num2 =3;
-let num3 =num1+num2
-console.log(`${num1}  + ${num2} is ${num3}`)
+let num1 = 2;
+let num2 = 3;
+let num3 = num1 + num2;
+console.log(`${num1}  + ${num2} is ${num3}`);
+
+//STRING METHODS
+
+//get the length of the string
+console.log(name.length);
+//To upper case
+console.log(fname.toUpperCase());
+
+//string slicing
+console.log(fname.slice(2, 4));
+console.log(fname.slice(1));
+// replace meyhod
+console.log(fname.replace("a","e"))
