60c21e4d9c51172022c01758dbc1b9f405a579b2
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-05-13 10:49:00 +0530

    parctice set

---
+++ src/JS/8-practice-arrays.js
@@ -0,0 +1,41 @@
+// create an Array that takes input from the user to add array in to it
+
+// let customArray = [];
+// let a;
+// for (let i = 0; i < 10; i++) {
+//   a = prompt("enter the number");
+//   a=Number.parseInt(a)
+//   customArray.push(a);
+//   console.log(customArray);
+// }
+// console.log(customArray);
+
+///example 2 go on adding numbers untill 0 is added
+// let addZero = [1, 2, 3, 7, 8, 4];
+// let b;
+// do {
+//   b = prompt("Enter the number");
+//   addZero.push(b);
+//   console.log(addZero);
+// } while (b != 0);
+
+//Filter for number divisible bty 10
+
+// let divArray = [20, 35, 25, 40, 50, 55, 45, 70];
+
+// let divnum = divArray.filter((value) => {
+//   //   if (value % 10 == 0) {
+//   return value % 10 == 0;
+//   //   }
+// });
+// console.log(divnum);
+
+//create an array of Square of a given numbers
+// let testArray=[1,2,3,4,5,6,7,8,9,10]
+// let squareArray= testArray.map((value)=>{
+//     return value*value
+// })
+// console.log(squareArray)
+
+
+//Use reduce to find the factorial of anumber
