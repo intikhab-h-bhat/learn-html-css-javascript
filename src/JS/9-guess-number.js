adbd69a7aaaaaf3bbbc8be43bcccbe0b8af876db
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-05-15 09:50:30 +0530

    guess number with chances

--- src/JS/9-guess-number.js
+++ src/JS/9-guess-number.js
@@ -1,19 +1,22 @@
 const random = Math.floor(Math.random() * 10);
 console.log(random);
-
+let chances = 0;
 let guessNumber = prompt("Guess The Number");
 
 while (guessNumber != random) {
   if (guessNumber < random) {
+    chances = chances + 1;
     guessNumber = prompt("Guess The Number again");
+   
     console.log("guessNumber is less than the random nuumber");
-  
   } else if (guessNumber > random) {
+    chances = chances + 1;
     guessNumber = prompt("Guess The Number again");
-    console.log("guessNumber is greater than the random nuumber");
     
+    console.log("guessNumber is greater than the random nuumber");
   }
 }
 if (guessNumber == random) {
   console.log("guessNumber is correct");
+  console.log(`No of chanse:  ${chances}`);
 }
