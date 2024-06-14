c14454bfd4cbab618dc58d27f9497bd77ccc5148
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-06-14 21:50:50 +0530

    cheecking node js

--- src/JS/10-consol-methods.js
+++ src/JS/10-consol-methods.js
@@ -7,3 +7,13 @@ console.warn("There is a warning");
 let list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.table(list1);
 
+
+// let a =prompt("Enter the Number","758")
+// let write=confirm("Do you really want to learn js")
+// if(write){
+//     console.log("yes")
+// }
+// else{
+//     console.log("No")
+// }
+    
