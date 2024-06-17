67309a07f2c0dff0cf18174134809c32b77fd262
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-06-17 22:12:22 +0530

    Program using Prompt and Alert

--- src/JS/11-DOM_BOM.js
+++ src/JS/11-DOM_BOM.js
@@ -1,1 +1,11 @@
-console.log(window)
+// console.log(window)
+// document.body.style.background = "red";
+let age = prompt("Enter age");
+age=Number.parseInt(age);
+if (age > 20){
+
+    alert("You are eligble for driving");
+}
+else {
+    alert("You are not eligble for driving");
+}
