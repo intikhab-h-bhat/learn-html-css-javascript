b53eef15a2beae8d403414892f6d2237a17b612f
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-06-17 22:25:26 +0530

    check for valid age

--- src/JS/11-DOM_BOM.js
+++ src/JS/11-DOM_BOM.js
@@ -2,10 +2,23 @@
 // document.body.style.background = "red";
 let age = prompt("Enter age");
 age=Number.parseInt(age);
-if (age > 20){
+
+if (age < 0){
+
+console.error("Please enter the valid value");
+}
+else {
+
+const CanDrive=(age)=>{
+    age>=20?true:false
+
+}
+
+if (CanDrive(age)){
 
     alert("You are eligble for driving");
 }
 else {
     alert("You are not eligble for driving");
+}
 }
