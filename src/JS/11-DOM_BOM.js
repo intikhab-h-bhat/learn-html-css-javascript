188b5ae977aa5f6d6e7fe73ad8a505a8d69bf95f
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-06-18 10:03:53 +0530

    changed the background color of oage from javascript

--- src/JS/11-DOM_BOM.js
+++ src/JS/11-DOM_BOM.js
@@ -1,24 +1,24 @@
 // console.log(window)
 // document.body.style.background = "red";
+
+let color = prompt("Enter the background color");
+document.body.style.background = color;
 let age = prompt("Enter age");
-age=Number.parseInt(age);
+age = Number.parseInt(age);
 
-if (age < 0){
 
-console.error("Please enter the valid value");
-}
-else {
+if (age < 0) {
+  console.error("Please enter the valid value");
+} else {
+    const CanDrive = (age) => {
+        age >= 20 ? true : false;
+      }
 
-const CanDrive=(age)=>{
-    age>=20?true:false
-
-}
-
-if (CanDrive(age)){
-
+  if (CanDrive(age)) {
     alert("You are eligble for driving");
-}
-else {
+  } else {
     alert("You are not eligble for driving");
+  }
 }
-}
+
+
