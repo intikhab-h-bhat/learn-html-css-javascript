2c03e6a51e471e13d9e2a8336e30f8c5d9808dd8
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-06-18 22:25:15 +0530

    Firstchild, lastchild,childnodes

--- src/JS/11-DOM_BOM.js
+++ src/JS/11-DOM_BOM.js
@@ -1,24 +1,32 @@
 // console.log(window)
 // document.body.style.background = "red";
 
-let color = prompt("Enter the background color");
-document.body.style.background = color;
-let age = prompt("Enter age");
-age = Number.parseInt(age);
+// let color = prompt("Enter the background color");
+// document.body.style.background = color;
+// let age = prompt("Enter age");
+// age = Number.parseInt(age);
 
 
- if (age < 0) {
- console.error("Please enter the valid value");
-} else {
-    const CanDrive = (age) => {
-        return age >= 20 ? true : false;
-      }
+//  if (age < 0) {
+//  console.error("Please enter the valid value");
+// } else {
+//     const CanDrive = (age) => {
+//         return age >= 20 ? true : false;
+//       }
 
-  if (CanDrive(age)) {
-    alert("You are eligble for driving");
-  } else {
-    alert("You are not eligble for driving");
-  }
-}
+//   if (CanDrive(age)) {
+//     alert("You are eligble for driving");
+//   } else {
+//     alert("You are not eligble for driving");
+//   }
+// }
+
+console.log(document.body.firstChild);
+console.log(document.body.firstChild.firstChild);
+console.log(document.body.lastChild);
+console.log(document.body.childNodes);
+console.log(document.body.firstChild.nextSibling);
+console.log(document.body.firstChild.nextElementSibling);
+
 
 
