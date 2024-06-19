c5cc324b317a27455a888913e0f4b9c9f32b35d3
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-06-19 19:09:34 +0530

    added some style and changed the background color

--- src/JS/11-DOM_BOM.js
+++ src/JS/11-DOM_BOM.js
@@ -1,36 +1,8 @@
-// console.log(window)
-// document.body.style.background = "red";
 
-// let color = prompt("Enter the background color");
-// document.body.style.background = color;
-// let age = prompt("Enter age");
-// age = Number.parseInt(age);
+const ChangeBgColor=()=>{
 
+    document.body.firstElementChild.style.background="red";
+}
 
-//  if (age < 0) {
-//  console.error("Please enter the valid value");
-// } else {
-//     const CanDrive = (age) => {
-//         return age >= 20 ? true : false;
-//       }
-
-//   if (CanDrive(age)) {
-//     alert("You are eligble for driving");
-//   } else {
-//     alert("You are not eligble for driving");
-//   }
-// }
-
-// console.log(document.body.firstChild);
-// console.log(document.body.firstChild.firstChild);
-// console.log(document.body.lastChild);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild.nextSibling);
-// console.log(document.body.firstChild.nextElementSibling);
-
-// to get elements child only 
-console.log(document.body.firstChild.nextElementSibling);
-console.log(document.body.firstChild.firstChild.nextElementSibling);
-
-
-
+let b = document.body;
+console.log("First child b is ", b.firstElementChild);
