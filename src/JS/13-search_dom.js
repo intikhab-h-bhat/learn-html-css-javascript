fe7b0ed4878465077d3bf40ea7379c991a103e76
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-06-20 21:24:27 +0530

    using getelement by id

--- src/JS/13-search_dom.js
+++ src/JS/13-search_dom.js
@@ -1,2 +1,5 @@
 let b = document.getElementsByClassName("card-title")[0];
 b.style.color="red";
+
+document.getElementById("cardtext").style.color= 'blue'
+document.getElementById("cardtext2").style.color= 'green'
