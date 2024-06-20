8c3de497fe29e44b4b202eecd05d81f6e6be1ed8
Intikhab Hussain Bhat <intikhabh@gmail.com>
2024-06-20 21:32:41 +0530

    using query selector all

--- src/JS/13-search_dom.js
+++ src/JS/13-search_dom.js
@@ -1,5 +1,10 @@
-let b = document.getElementsByClassName("card-title")[0];
-b.style.color="red";
+// let b = document.getElementsByClassName("card-title")[0];
+// b.style.color="red";
 
 document.getElementById("cardtext").style.color= 'blue'
 document.getElementById("cardtext2").style.color= 'green'
+
+let c = document.querySelectorAll(".card-title")
+c[0].style.color= "red";
+c[1].style.color= "blue";
+c[2].style.color= "green";
